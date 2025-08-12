2e28285eda1ec28b160d93f3a90932ded7d15053
Butcha82 <lee.butcher1982@gmail.com>
2025-08-12 16:24:39 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // keep pdf2json out of the bundle
-      const externals = config.externals || [];
-      config.externals = [
-        (...args) => {
-          const callback = args[2];
-          const req = args[1];
-          if (typeof req === "string" && req === "pdf2json") {
-            return callback(null, "commonjs pdf2json");
-          }
-          return externals[0] ? externals[0](...args) : callback();
-        },
-      ];
-    }
-    return config;
-  },
+  // Ensure this package is included in the serverless function
+  serverExternalPackages: ['pdf2json'],
 };
