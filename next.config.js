4694eda10ee92c56e06bf87f2b36a67ad8e72a2f
Butcha82 <lee.butcher1982@gmail.com>
2025-08-12 13:51:44 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    // Don’t try to bundle node-canvas (not needed for text extraction)
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      canvas: false,
+      'canvas-prebuilt': false,
+      'pdfjs-dist/build/pdf.worker.js': false,
+      'pdfjs-dist/legacy/build/pdf.worker.js': false,
+    };
+    return config;
+  },
+};
 module.exports = nextConfig;
