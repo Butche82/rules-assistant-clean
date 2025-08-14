8de82058fa7c913cabff2252593ea4ecdac5c801
Butcha82 <lee.butcher1982@gmail.com>
2025-08-14 20:12:52 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Make sure the package is present on the server at runtime (but not bundled)
+module.exports = {
+  // Make sure pdf2json is present on the lambda, resolved via native Node require
   serverExternalPackages: ['pdf2json'],
+  // Belt-and-braces include for anything the tracer misses
   outputFileTracingIncludes: {
     '/': ['node_modules/pdf2json/**'],
   },
 };
-module.exports = nextConfig;
